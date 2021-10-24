74d530321e878a4338fd35778a6fba06ae8e487c
MCF512 <egdem4enko@yandex.ru>
2021-10-24 23:56:17 +0300

    not ready responsive

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,20 @@
 $(function () {
 
 });
+
+$('.partners__slider').slick({
+  dots: true,
+  responsive: [{
+
+    breakpoint: 769,
+    settings: {
+      unslick
+    }
+  }
+  ]
+});
+
+$('.header__burger').on('click', function () {
+  $('.header__nav').toggleClass('header__nav--active');
+  $('body').toggleClass('body--fixed');
+})

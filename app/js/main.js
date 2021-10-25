e74d73b6f70f5ab7a7f5dd54b034323280268eff
MCF512 <egdem4enko@yandex.ru>
2021-10-25 16:11:43 +0300

    done

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,16 @@ $(function () {
 
 });
 
+$('.header__burger').on('click', function () {
+  $('.header__nav').toggleClass('header__nav--active');
+  $('body').toggleClass('body--fixed');
+});
+
+$('.header__link').on('click', function () {
+  $('.header__nav').removeClass('header__nav--active');
+  $('body').removeClass('body--fixed');
+});
+
 $('.partners__slider').slick({
   dots: true,
   responsive: [{
@@ -14,7 +24,4 @@ $('.partners__slider').slick({
   ]
 });
 
-$('.header__burger').on('click', function () {
-  $('.header__nav').toggleClass('header__nav--active');
-  $('body').toggleClass('body--fixed');
-})
+
